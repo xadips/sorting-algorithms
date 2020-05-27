48744fb8a5b5a661aeb115551e201835b80a5f7c
Spidax <spidax1881@gmail.com>
2020-05-28 01:40:17 +0300

    Implemented sorting algorithms

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "sort.hpp"
+
+template<class T>
+void printArray(T arr[], size_t size)
+{
+    for(size_t i = 0; i < size; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << "\n";
+}
+
+
+int main()
+{
+    int arr[] = {4, 5, 2, 1, 0};
+    size_t size = 5;
+    printArray(arr, size);
+    //quickSort(arr, size);
+    hybridSort(arr, size);
+    printArray(arr, size);
+    return 0;
+}
